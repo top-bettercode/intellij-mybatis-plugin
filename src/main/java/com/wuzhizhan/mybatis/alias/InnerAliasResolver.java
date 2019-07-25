6d017f7f571eac218ceaa7d687f012218be51c82
Peter Wu <piterwu@outlook.com>
2019-07-25 15:07:35 +0800

    异常兼容

--- src/main/java/com/wuzhizhan/mybatis/alias/InnerAliasResolver.java
+++ src/main/java/com/wuzhizhan/mybatis/alias/InnerAliasResolver.java
@@ -1,9 +1,12 @@
 package com.wuzhizhan.mybatis.alias;
 
-import com.google.common.collect.ImmutableSet;
+import com.google.common.base.Optional;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.wuzhizhan.mybatis.util.JavaUtils;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,30 +16,39 @@ import org.jetbrains.annotations.Nullable;
  */
 public class InnerAliasResolver extends AliasResolver {
 
-  private final Set<AliasDesc> innerAliasDescs = ImmutableSet.of(
-      AliasDesc.create(JavaUtils.findClazz(project, "java.lang.String").orNull(), "string"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.lang.Byte").orNull(), "byte"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.lang.Long").orNull(), "long"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.lang.Short").orNull(), "short"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.lang.Integer").orNull(), "int"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.lang.Integer").orNull(), "integer"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.lang.Double").orNull(), "double"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.lang.Float").orNull(), "float"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.lang.Boolean").orNull(), "boolean"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.util.Date").orNull(), "date"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.math.BigDecimal").orNull(), "decimal"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.lang.Object").orNull(), "object"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.util.Map").orNull(), "map"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.util.HashMap").orNull(), "hashmap"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.util.List").orNull(), "list"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.util.ArrayList").orNull(), "arraylist"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.util.Collection").orNull(), "collection"),
-      AliasDesc.create(JavaUtils.findClazz(project, "java.util.Iterator").orNull(), "iterator")
-  );
+  private final Set<AliasDesc> innerAliasDescs;
 
   public InnerAliasResolver(Project project) {
     super(project);
-    innerAliasDescs.remove(null);
+    Set<AliasDesc> aliasDescs = new HashSet<>();
+    addAliasDesc(project, aliasDescs, "java.lang.String", "string");
+    addAliasDesc(project, aliasDescs, "java.lang.Byte", "byte");
+    addAliasDesc(project, aliasDescs, "java.lang.Long", "long");
+    addAliasDesc(project, aliasDescs, "java.lang.Short", "short");
+    addAliasDesc(project, aliasDescs, "java.lang.Integer", "int");
+    addAliasDesc(project, aliasDescs, "java.lang.Integer", "integer");
+    addAliasDesc(project, aliasDescs, "java.lang.Double", "double");
+    addAliasDesc(project, aliasDescs, "java.lang.Float", "float");
+    addAliasDesc(project, aliasDescs, "java.lang.Boolean", "boolean");
+    addAliasDesc(project, aliasDescs, "java.util.Date", "date");
+    addAliasDesc(project, aliasDescs, "java.math.BigDecimal", "decimal");
+    addAliasDesc(project, aliasDescs, "java.lang.Object", "object");
+    addAliasDesc(project, aliasDescs, "java.util.Map", "map");
+    addAliasDesc(project, aliasDescs, "java.util.HashMap", "hashmap");
+    addAliasDesc(project, aliasDescs, "java.util.List", "list");
+    addAliasDesc(project, aliasDescs, "java.util.ArrayList", "arraylist");
+    addAliasDesc(project, aliasDescs, "java.util.Collection", "collection");
+    addAliasDesc(project, aliasDescs, "java.util.Iterator", "iterator");
+    innerAliasDescs = Collections.unmodifiableSet(aliasDescs);
+  }
+
+  @NotNull
+  private void addAliasDesc(Project project, Set<AliasDesc> aliasDescs, String clazzName,
+      String alias) {
+    Optional<PsiClass> clazz = JavaUtils.findClazz(project, clazzName);
+    if (clazz.isPresent()) {
+      aliasDescs.add(AliasDesc.create(clazz.get(), alias));
+    }
   }
 
   @NotNull
